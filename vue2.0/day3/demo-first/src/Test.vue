b7f9adf58e3b39931f9fc1bb0e4e58873cf6931a
Cyshell <chen19981011@outlook.com>
2023-02-11 01:13:28 +0800

    #

---
+++ vue2.0/day3/demo-first/src/Test.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="test-box">
+      <h3>这是用户自定义的Test.vue --- {{ username }}</h3>
+      <button @click="changeName">修改用户名</button>
+    </div>
+    <div>123</div>
+  </div>
+</template>
+
+<script>
+// 默认导出，这是固定写法！
+export default {
+  // data 数据源
+  // 注意：.vue组件中的data不能像之前一样指向对象。
+  // 注意：组件中的data必须是一个函数
+  data() {
+    return {
+      username: "admin",
+    };
+  },
+  methods: {
+    changeName() {
+      // 在组件中，this就表示当前组件的实例对象
+      this.username = "娃哈哈";
+    },
+  },
+  //当前组件中的侦听器
+  watch: {},
+  //   当前组件中的计算属性
+  computed: {},
+  // 当前组件中的过滤器
+  filters: {},
+};
+</script>
+
+<style>
+</style>
+
+<style lang="less">
+.test-box {
+  background-color: pink;
+  h3 {
+    color: red;
+  }
+}
+</style>
--- vue2.0/day3/demo-first/src/main.js
+++ vue2.0/day3/demo-first/src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+//导入vue这个包，得到Vue构造函数
+import Vue from "vue";
+// 导入App.vue根组件，将来要把App.vue中的模板结构，渲染到HTML页面中
+import App from "./App.vue";
+import Test from "./Test.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+// 创建Vue的实例对象
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  // 把render函数指定的组件,渲染到HTML页面中
+  // render函数中渲染的是哪个组件 哪个组件就是根组件
+  render: (h) => h(Test),
+}).$mount("#app");
+
+// Vue实例的$mount()方法，作用和el属性完全一样
